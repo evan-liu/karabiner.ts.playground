79482d3c6dab20c2b8353a478fe9345f7c108746
evan-liu <evancoding@gmail.com>
2023-05-20 17:36:16 +1200

    Add more imports from karabiner.ts

--- rules.js
+++ rules.js
@@ -1,13 +1,32 @@
 import {
+  // rule and layers
   rule,
   layer,
   simlayer,
+  hyperLayer,
+  modifierLayer,
   duoLayer,
+  // from / map()
   map,
+  mapConsumerKey,
+  mapPointingButton,
   mapSimultaneous,
   mapDoubleTap,
+  // to
   toKey,
-  complexModifications,
+  toConsumerKey,
+  toPointingButton,
+  toHyper,
+  to$,
+  toApp,
+  toPaste,
+  // conditions
+  ifApp,
+  ifDevice,
+  ifVar,
+  // utils
+  withCondition,
+  withMapper,
 } from 'karabiner.ts'
 
 export const rules = [
