c90118516361c57a5ed1acae994c17a29a7491bb
Evan Liu <evancoding@gmail.com>
2023-12-28 10:30:45 +1300

    ✨ Add copy button

--- main.js
+++ main.js
@@ -14,9 +14,11 @@ try {
 
 document.querySelector('#app').innerHTML = `\
 <div>
+  <button id="copy" data-clipboard-target="#output">Copy</button>
+  
   <a href="https://karabiner-elements.pqrs.org/docs/json/complex-modifications-manipulator-definition/" target="_blank">
-    🔗 Karabiner Elements Manipulator Definition
+    ? Manipulator Definition Reference
   </a>
-      
-  <pre${error ? ' class="error"' : ''}>${error || code}</pre>
+  
+  <pre id="output" ${error ? ' class="error"' : ''}>${error || code}</pre>
 </div>`
